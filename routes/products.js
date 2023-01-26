808d1f9e945845e1ba5acb418ed8b26893684caa
bubi1600 <burak97@live.se>
2023-01-26 08:14:09 +0100

    workflow to publish package

--- routes/products.js
+++ routes/products.js
@@ -54,26 +54,21 @@ router.get(`/:id`, async (req, res) => {
 });
 
 router.post(`/create`, uploadOptions.single('image'), async (req, res) => {
-    const category = await Category.findById(req.body.category);
-    if (!category) return res.status(400).send('Invalid Category');
+    //const category = await Category.findById(req.body.category);
+    //if (!category) return res.status(400).send('Invalid Category');
 
-    const file = req.file;
-    if (!file) return res.status(400).send('No image in the request');
+    //const file = req.file;
+    //if (!file) return res.status(400).send('No image in the request');
 
-    const fileName = file.filename;
-    const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
+    //const fileName = file.filename;
+    //const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
     let product = new Product({
         name: req.body.name,
         description: req.body.description,
-        richDescription: req.body.richDescription,
-        image: `${basePath}${fileName}`, // "http://localhost:3000/public/upload/image-2323232"
+        //image: `${basePath}${fileName}`, // "http://localhost:3000/public/upload/image-2323232"
         brand: req.body.brand,
         price: req.body.price,
-        category: req.body.category,
-        countInStock: req.body.countInStock,
-        rating: req.body.rating,
-        numReviews: req.body.numReviews,
-        isFeatured: req.body.isFeatured,
+        //category: req.body.category,
     });
 
     product = await product.save();
