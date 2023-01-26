5d90d240c5f9368d812a230110b64d319c49521a
bubi1600 <burak97@live.se>
2023-01-26 08:41:39 +0100

    workflow to publish package

--- routes/products.js
+++ routes/products.js
@@ -57,15 +57,15 @@ router.post(`/create`, uploadOptions.single('image'), async (req, res) => {
     //const category = await Category.findById(req.body.category);
     //if (!category) return res.status(400).send('Invalid Category');
 
-    //const file = req.file;
-    //if (!file) return res.status(400).send('No image in the request');
+    const file = req.file;
+    if (!file) return res.status(400).send('No image in the request');
 
-    //const fileName = file.filename;
-    //const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
+    const fileName = file.filename;
+    const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
     let product = new Product({
         name: req.body.name,
         description: req.body.description,
-        //image: `${basePath}${fileName}`, // "http://localhost:3000/public/upload/image-2323232"
+        image: `${basePath}${fileName}`, // "http://localhost:3000/public/upload/image-2323232"
         brand: req.body.brand,
         price: req.body.price,
         //category: req.body.category,
