b9ebfe153078fa237aca588f6a09204fd471a848
bubi1600 <burak97@live.se>
2023-01-28 21:47:32 +0100

    workflow to publish package

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.options('*', cors())
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(authJwt());
-app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
+app.use('/tmp', express.static(__dirname + '/tmp'));
 app.use(errorHandler);
 
 //Routes
