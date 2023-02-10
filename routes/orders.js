9866cbdc027849fcbdaed729e4d3fba41c84f8f2
bubi1600 <burak97@live.se>
2023-02-10 19:43:10 +0100

    workflow to publish package

--- routes/orders.js
+++ routes/orders.js
@@ -136,7 +136,7 @@ router.get(`/:userID`, async (req, res) => {
     if (!userOrderList) {
         res.status(500).json({ success: false })
     }
-    res.json({ userOrderList });
+    res.json({ userOrderList, count: orders.length });
 })
 
 
