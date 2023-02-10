fa6951d00764e41f0cb4ddb212b0430c98db2891
bubi1600 <burak97@live.se>
2023-02-10 15:28:31 +0100

    workflow to publish package

--- routes/products.js
+++ routes/products.js
@@ -54,9 +54,11 @@ router.get(`/read/:productID`, async (req, res) => {
 });
 
 router.post(`/create`, uploadOptions.single('image'), async (req, res) => {
-
-    const category = await Category.findById(req.body.category);
-    if (!category) return res.status(400).send('Invalid Category');
+    if (id.match(/^[0-9a-fA-F]{24}$/)) {
+        // Yes, it's a valid ObjectId, proceed with `findById` call.
+        const category = await Category.findById(req.body.category);
+        if (!category) return res.status(400).send('Invalid Category');
+    }
 
     const file = req.file;
     if (!file) return res.status(400).send('No image in the request');
