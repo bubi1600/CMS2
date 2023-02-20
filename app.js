c6c05e8b815f00310e81d3611c22df8fd8faf558
bubi1600 <burak97@live.se>
2023-02-20 18:06:20 +0100

    workflow to publish package

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ const categoriesRoutes = require('./routes/categories');
 const productsRoutes = require('./routes/products');
 const usersRoutes = require('./routes/users');
 const ordersRoutes = require('./routes/orders');
+const inventoriesRoutes = require('./routes/inventory-aggregate');
 
 const api = process.env.API_URL;
 const PORT = process.env.PORT || 3000
@@ -31,6 +32,7 @@ app.use(`${api}/categories`, categoriesRoutes);
 app.use(`${api}/products`, productsRoutes);
 app.use(`${api}/users`, usersRoutes);
 app.use(`${api}/orders`, ordersRoutes);
+app.use(`${api}/inventories`, inventoriesRoutes);
 
 //Database
 mongoose.connect(process.env.CONNECTION_STRING, {
