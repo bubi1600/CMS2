49e10befdfeac937b3c935ef5a92387f4e0ba6fe
bubi1600 <burak97@live.se>
2023-03-06 13:46:46 +0100

    workflow to publish package

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(`${api}/categories`, categoriesRoutes);
 app.use(`${api}/products`, productsRoutes);
 app.use(`${api}/users`, usersRoutes);
 app.use(`${api}/orders`, ordersRoutes);
-app.use(`/productQuantities`, productQuantitiesRoutes);
+app.use(`${api}/productQuantities`, productQuantitiesRoutes);
 
 //Database
 mongoose.connect(process.env.CONNECTION_STRING, {
