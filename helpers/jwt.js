47693ccb1b4faade97ce88c36b2dc129439b5fb6
bubi1600 <burak97@live.se>
2023-03-06 14:13:08 +0100

    workflow to publish package

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -9,18 +9,19 @@ function authJwt() {
         isRevoked: isRevoked
     }).unless({
         path: [
-            {url: /\/public\/uploads(.*)/ , methods: ['GET', 'OPTIONS'] },
-            {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
-            {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
-            {url: /\/api\/v1\/orders(.*)/,methods: ['GET', 'OPTIONS', 'POST']},
+            { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/orders(.*)/, methods: ['GET', 'OPTIONS', 'POST'] },
             `${api}/users/login`,
             `${api}/users/register`,
+            `${api}/productQuantities/`,
         ]
     })
 }
 
 async function isRevoked(req, payload, done) {
-    if(!payload.isAdmin) {
+    if (!payload.isAdmin) {
         done(null, true)
     }
 
