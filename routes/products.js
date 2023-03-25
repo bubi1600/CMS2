f343929874e1a375f90dcea0b78c89457dc049dd
bubi1600 <burak97@live.se>
2023-03-25 18:12:03 +0100

    workflow to publish package

--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { Category } = require('../models/category');
 const router = express.Router();
 const mongoose = require('mongoose');
-//const multer = require('multer');
+const multer = require('multer');
 const path = require('path');
 
 /*const FILE_TYPE_MAP = {
