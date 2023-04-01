fda7607b56a0e8da5698e3813f5a0eb1a47e8b17
bubi1600 <burak97@live.se>
2023-04-01 07:21:19 +0200

    workflow to publish package

--- routes/categories.js
+++ routes/categories.js
@@ -13,7 +13,7 @@ router.get(`/`, async (req, res) => {
             res.status(500).json({ success: false })
         }
 
-        res.status(200).json({ categories });
+        res.status(200).json({ success: true, categories });
     } catch (error) {
         console.error(error);
         res.status(500).json({ success: false, error: 'Error retrieving categories' })
@@ -29,7 +29,7 @@ router.get('/:userId', async (req, res) => {
         }
 
         const category = user.category;
-        res.status(200).json({ category });
+        res.status(200).json({ success: true, category });
 
     } catch (error) {
         console.error(error);
