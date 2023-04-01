279e197cc9d0d8b229976da23ff6e7417ee04ce3
bubi1600 <burak97@live.se>
2023-04-01 07:48:11 +0200

    workflow to publish package

--- routes/categories.js
+++ routes/categories.js
@@ -28,7 +28,7 @@ router.get('/:userId', async (req, res) => {
             return res.status(404).json({ success: false, message: 'User not found' });
         }
 
-        res.status(200).json({ success: true, category: user.category });
+        res.status(200).json({ success: true, categories: user.category });
 
     } catch (error) {
         console.error(error);
