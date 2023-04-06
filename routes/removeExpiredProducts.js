027942d54c2386488babc39148ca1331810ae3e0
bubi1600 <burak97@live.se>
2023-04-07 00:39:43 +0200

    workflow to publish package

--- routes/removeExpiredProducts.js
+++ routes/removeExpiredProducts.js
@@ -1,18 +1,29 @@
 const { Product } = require('../models/product');
+const { ProductQuantity } = require('../models/productQuantity');
 
 async function removeExpiredProducts() {
     try {
         // Find all products that are expired
         const expiredProducts = await Product.find({
-            expiryDate: { $lte: new Date() }
+            expiryDate: { $lte: new Date() },
         });
 
-        // Remove expired products
+        // Find all product quantities associated with the expired products
+        const expiredProductQuantities = await ProductQuantity.find({
+            product: { $in: expiredProducts.map((p) => p._id) },
+        });
+
+        // Remove the product quantities
+        for (const productQuantity of expiredProductQuantities) {
+            await productQuantity.remove();
+        }
+
+        // Remove the expired products
         for (const product of expiredProducts) {
             await product.remove();
         }
 
-        console.log(`Removed ${expiredProducts.length} expired products`);
+        console.log(`Removed ${expiredProducts.length} expired products and ${expiredProductQuantities.length} product quantities`);
     } catch (error) {
         console.error(`Error removing expired products: ${error}`);
     }
