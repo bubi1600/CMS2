d0f6845b1d04108a7ccd9408bb9957e86c88226e
bubi1600 <burak97@live.se>
2023-04-07 01:22:55 +0200

    workflow to publish package

--- routes/removeExpiredProducts.js
+++ routes/removeExpiredProducts.js
@@ -4,7 +4,11 @@ const { ProductQuantity } = require('../models/productQuantity');
 async function removeExpiredProducts() {
     try {
         // Find all products that are expired
-        const expiredProducts = await Product.find({ expiryDate: { $lte: new Date() } });
+        const expiredProducts = await Product.find(
+            { expiryDate: { $lte: new Date() } },
+            null,
+            { timeout: 30000 } // increase timeout to 30 seconds
+        );
 
         // Remove expired products and their associated product quantities
         for (const product of expiredProducts) {
