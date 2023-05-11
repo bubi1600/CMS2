9034bbe942936fb570248ef3b6c2727e3cd7441a
bubi1600 <burak97@live.se>
2023-05-11 17:13:20 +0200

    workflow to publish package

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -14,6 +14,7 @@ function authJwt() {
             { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
             { url: /\/api\/v1\/orders(.*)/, methods: ['GET', 'OPTIONS', 'POST'] },
             { url: /\/api\/v1\/productQuantities(.*)/, methods: ['GET', 'OPTIONS', 'DELETE'] },
+            { url: /\/api\/v1\/test(.*)/, methods: ['GET', 'POST', 'OPTIONS'] },
             `${api}/users/login`,
             `${api}/users/register`,
             `https://cmstwo.cyclic.app/api/v1/productQuantities/63c705b7f3794300239ca613/63e65e061142c1002c96e8cc`,
